5ccdf22546de429b0350d767200a5476de5ac0e3
narcis.fanica <narcis.fanica@getik.net>
2023-03-15 00:34:10 +0200

    Modified algorithms, changed 3

--- L1_1.py
+++ L1_1.py
@@ -1,5 +1,5 @@
 def determinant(x1, y1, x2, y2, x3, y3):
-    return 1*(x1*y2 + x2*y3 + x3*y1) - x1*y3 - x2*y1 - x3*y2
+    return x2*y3 + x3*y1 + x1*y2 - x2*y1 - x3*y2 - x1*y3 
 
 t = int(input())
 lista_rezultate = []
--- L1_3.py
+++ L1_3.py
@@ -1,36 +1,28 @@
 def determinant(x1, y1, x2, y2, x3, y3):
-    return 1*(x1*y2 + x2*y3 + x3*y1) - x1*y3 - x2*y1 - x3*y2
+    return x2*y3 + x3*y1 + x1*y2 - x2*y1 - x3*y2 - x1*y3     
 
-def get_start_point(lista_puncte):
-    min_point = min(lista_puncte, key=lambda x: (x[1], x[0]))
-    return lista_puncte.index(min_point)
+n = int(input())
+points = [] 
 
-def acoperire_convexa(lista_puncte, min_point):
-    current = min_point
-    acoperire = [lista_puncte[current]]
-    while True:
-        succesor = (current + 1) % len(lista_puncte)
-        for i in range(len(lista_puncte)):
-            if determinant(lista_puncte[current][0], lista_puncte[current][1], lista_puncte[i][0], lista_puncte[i][1], lista_puncte[succesor][0], lista_puncte[succesor][1]) < 0:
-                succesor = i
-        if succesor == min_point:
-            break
-        else:
-            current = succesor
-            acoperire.append(lista_puncte[succesor])
-    return acoperire[::-1]
-    
-t = int(input())
-lista_puncte = [] 
-
-for i in range(t):
+for i in range(n):
     x1, y1 = map(int, input().split())
-    lista_puncte.append((x1, y1))
-#print(get_start_point(lista_puncte))
-acoperire = acoperire_convexa(lista_puncte, get_start_point(lista_puncte))
+    points.append((x1, y1))
+acoperire = [points[0], points[1]]
+
+for i in range(2, n):
+        acoperire.append(points[i])
+        while len(acoperire)>2 and determinant(acoperire[-3][0], acoperire[-3][1], acoperire[-2][0], acoperire[-2][1], acoperire[-1][0], acoperire[-1][1])<=0: 
+            del acoperire[-2]
+while len(acoperire)>2 and determinant(acoperire[-2][0], acoperire[-2][1], acoperire[-1][0], acoperire[-1][1], acoperire[0][0], acoperire[0][1])<=0:
+    del acoperire[-1]
+while len(acoperire)>2 and determinant(acoperire[-1][0], acoperire[-1][1], acoperire[0][0], acoperire[0][1], acoperire[1][0], acoperire[1][1])<=0:
+    del acoperire[0]
+while len(acoperire)>2 and determinant(acoperire[0][0], acoperire[0][1], acoperire[1][0], acoperire[1][1], acoperire[2][0], acoperire[2][1])<=0:
+    del acoperire[1]
+    
 print(len(acoperire))
 for i in acoperire:
     print(*i)
-    
-    
+
+
 
