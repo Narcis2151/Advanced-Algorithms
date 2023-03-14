eb1e6908741b61ce5fb5974e50239d4c47ad1008
narcis.fanica <narcis.fanica@getik.net>
2023-03-14 21:28:14 +0200

    Finished 1st lab

--- L1_3.py
+++ L1_3.py
@@ -2,29 +2,23 @@ def determinant(x1, y1, x2, y2, x3, y3):
     return 1*(x1*y2 + x2*y3 + x3*y1) - x1*y3 - x2*y1 - x3*y2
 
 def get_start_point(lista_puncte):
-    start_point = min(lista_puncte, key=lambda x: (x[1], x[0]))
-    return lista_puncte.index(start_point)
-    
-def acoperire_convexa(lista_puncte, index_min):
+    min_point = min(lista_puncte, key=lambda x: (x[1], x[0]))
+    return lista_puncte.index(min_point)
 
-    l_inf = [lista_puncte[index_min], lista_puncte[index_min + 1]]
-    index_min += 2
-    
-    for i in range(2, len(lista_puncte)):
-        
-        if index_min == len(lista_puncte):
-            index_min = 0
-            
-        l_inf.append(lista_puncte[index_min]) 
-        print(l_inf)
-        
-        while len(l_inf) > 2 and determinant(l_inf[-3][0], l_inf[-3][1], l_inf[-2][0], l_inf[-2][1], l_inf[-1][0], l_inf[-1][1]) <= 0:
-            l_inf.pop(-2)
-            
-        index_min += 1
-    return l_inf
-
-
+def acoperire_convexa(lista_puncte, min_point):
+    current = min_point
+    acoperire = [lista_puncte[current]]
+    while True:
+        succesor = (current + 1) % len(lista_puncte)
+        for i in range(len(lista_puncte)):
+            if determinant(lista_puncte[current][0], lista_puncte[current][1], lista_puncte[i][0], lista_puncte[i][1], lista_puncte[succesor][0], lista_puncte[succesor][1]) < 0:
+                succesor = i
+        if succesor == min_point:
+            break
+        else:
+            current = succesor
+            acoperire.append(lista_puncte[succesor])
+    return acoperire[::-1]
     
 t = int(input())
 lista_puncte = [] 
@@ -33,7 +27,10 @@ for i in range(t):
     x1, y1 = map(int, input().split())
     lista_puncte.append((x1, y1))
 #print(get_start_point(lista_puncte))
-acoperire = acoperire_convexa(lista_puncte, index_min = get_start_point(lista_puncte))
+acoperire = acoperire_convexa(lista_puncte, get_start_point(lista_puncte))
 print(len(acoperire))
 for i in acoperire:
     print(*i)
+    
+    
+
